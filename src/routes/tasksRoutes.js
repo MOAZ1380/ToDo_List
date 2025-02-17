a7e2b07844f675bb1f4d6b93c39f98af70f3b544
Moaz sayed <moaz469654@gmail.com>
2025-02-17 18:57:08 +0200

    delete task

---
+++ src/routes/tasksRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { getTasks, getTask, createTask, updateTask, Delete_Task } = require('../controllers/taskController');
+
+const router = express.Router();
+
+router.post('/', createTask);
+router.get('/', getTasks);
+router.get('/:id', getTask)
+router.patch('/:id', updateTask)
+router.patch('/:id', Delete_Task)
+
+
+module.exports = router;
